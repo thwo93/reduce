214b8b9247476a771a5ed4378450e03c42d7f6aa
thwo93 <cos38zb@outlook.com>
2024-06-25 23:19:43 +0800

    Modify happen.cpp

--- happen.cpp
+++ happen.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Lot employee get happen ready.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
