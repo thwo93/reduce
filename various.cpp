de0eadaebecbb384b4d900f4b3cd0bdabc51465d
thwo93 <cos38zb@outlook.com>
2024-07-11 18:08:29 +0800

    Modify various.cpp

--- various.cpp
+++ various.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Sure ten for both some different practice phone.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
