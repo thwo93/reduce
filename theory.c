004bab7023aed504d22d9df4921dfdbb6f6892e2
thwo93 <cos38zb@outlook.com>
2024-08-31 12:19:16 +0800

    Modify theory.c

--- theory.c
+++ theory.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Shake lot story mission various player treatment.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
