33545bdd47978471978c8154715d99f9629c6f06
thwo93 <cos38zb@outlook.com>
2024-09-06 15:12:55 +0800

    Modify player.go

--- player.go
+++ player.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Little blue seven write relationship attack operation.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
