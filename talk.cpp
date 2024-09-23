aaddc24f1516dfdb709e76b27da6c71006138e58
thwo93 <cos38zb@outlook.com>
2024-09-23 12:02:14 +0800

    Modify talk.cpp

--- talk.cpp
+++ talk.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# News lay candidate recognize moment single officer program.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
