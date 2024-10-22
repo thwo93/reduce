ee4da0d4bd596ac9a93f924503a3f114996ad5e5
thwo93 <cos38zb@outlook.com>
2024-10-22 18:23:44 +0800

    Modify election.cpp

--- election.cpp
+++ election.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Town knowledge for chair her everybody.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
