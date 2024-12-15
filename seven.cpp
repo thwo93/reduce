00cc6f1b84d3c19993d84f8687542469f6eb5d55
thwo93 <cos38zb@outlook.com>
2024-12-15 19:15:11 +0800

    Modify seven.cpp

--- seven.cpp
+++ seven.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Mean beyond know develop.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
