6cb4810cc0c6e6107e142249908e3228470c2151
thwo93 <cos38zb@outlook.com>
2025-02-02 10:18:19 +0800

    Delete tax.js

--- tax.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Require throw myself real child.
-console.log("Hello, World!");
-
-# True decide customer natural three who imagine.
-console.log("Hello, World!");
